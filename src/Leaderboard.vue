6c6fc895869a37b3068fe1fc9a5b467f1f9da75e
Ruslan <kinrany@yandex.com>
2018-06-03 19:51:08 +0300

    Create Leaderboard component

---
+++ src/Leaderboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>Leaderboard</h3>
+    <span v-for="record of topRecords" :key="record.name">
+      {{ record.name }} | Level: {{ record.level }} | Score: {{ record.score }} <br/>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "v-leaderboard",
+  props: {
+    records: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    topRecords() {
+      return this.records
+        .slice()
+        .sort((a, b) => b.score - a.score)
+        .slice(0, 10);
+    }
+  }
+};
+</script>
